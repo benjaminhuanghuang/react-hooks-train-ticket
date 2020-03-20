fbc53a66a319c52674c422e7689cf5b64afff37c
Benjamin Huang <bhuang@rms.com>
2020-03-20 08:13:11 -0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 //
 import ContextDemoApp from "./ContextDemoApp"
+import ContextTypeDemoApp from "./ContextTypeDemoApp"
 
 
-
-ReactDOM.render(<ContextDemoApp />, document.getElementById('root'));
+ReactDOM.render(<ContextTypeDemoApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
