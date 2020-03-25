78c1f7c3476c13520f982543bfe29fd6c4415f59
Benjamin Huang <bhuang@rms.com>
2020-03-24 18:58:54 -0700

    copy code

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-//
-import ContextDemoApp from "./ContextDemoApp"
-import ContextTypeDemoApp from "./ContextTypeDemoApp"
-import LazyDemoApp from "./LazyDemoApp"
-import MemoDemoApp from "./MemoDemoApp"
-import HookDemoApp from "./StateHookDemoApp"
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "normalize.css/normalize.css";
+import * as serviceWorker from "../serviceWorker";
 
+import store from "./store";
+import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.render(<HookDemoApp />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if ("production" === process.env.NODE_ENV) {
+  serviceWorker.register();
+} else {
+  serviceWorker.unregister();
+}
